5d42254cabd15cd5568008dcc049cf4fdc39c0ca
Steven Gonzalez <110998322+Gonsteven11@users.noreply.github.com>
2022-10-31 18:38:16 -0500

    Add files via upload
    Adding in unemployment data for the current year. Also including the labor force data.

---
+++ BLS_API_Test.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import prettytable
+headers = {'Content-type': 'application/json'}
+data = json.dumps({"seriesid": ['LNS11000000','LNS13000000'],"startyear":"2013", "endyear":"2022"})
+p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+json_data = json.loads(p.text)
+for series in json_data['Results']['series']:
+    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+    seriesId = series['seriesID']
+    for item in series['data']:
+        year = item['year']
+        period = item['period']
+        value = item['value']
+        footnotes=""
+        for footnote in item['footnotes']:
+            if footnote:
+                footnotes = footnotes + footnote['text'] + ','
+        if 'M01' <= period <= 'M12':
+            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+    output = open(seriesId + '.csv','w')
+    output.write (x.get_string())
+output.close()
